cf064f818a0d2e40c1d08c566c2ebd32b505e81f
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2022-01-09 21:51:41 +0100

    Update all page

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
 const nav = document.querySelector(".nav");
-const navBtn = document.querySelector(".burger-btn");
-const allNavItems = document.querySelectorAll(".nav__item");
-
-const handleNav = () => {
-  nav.classList.toggle("nav--active");
-  allNavItems.forEach((item) => {
-    item.addEventListener("click", () => {
-      nav.classList.remove("nav--active");
-    });
-  });
-  
-};
-
-const handleNavItemsAnimation = () => {
-  let delayTime = 0;
-
-  allNavItems.forEach((item) => {
-    item.classList.toggle("nav-items-animation");
-    item.style.animationDelay = "." + delayTime + "s";
-    delayTime++;
-  });
-};
-handleNavItemsAnimation();
-navBtn.addEventListener("click", handleNav);
-
+      const navBtn = document.querySelector(".burger-btn");
+      const allNavItems = document.querySelectorAll(".nav__item");
+      
+      const handleNav = () => {
+        nav.classList.toggle("nav--active");
+        allNavItems.forEach((item) => {
+          item.classList.toggle("nav-items-animation");
+        });
+        allNavItems.forEach((item) => {
+          item.addEventListener("click", () => {
+            nav.classList.remove("nav--active");
+            allNavItems.forEach((item) => {
+              item.classList.remove("nav-items-animation");
+            });
+          });
+        });
+        
+      };
+      
+      const handleNavItemsAnimation = () => {
+        let delayTime = 0;
+      
+        allNavItems.forEach((item) => {
+          item.style.animationDelay = '.' + (delayTime + 2) + "s";
+          delayTime++;
+        });
+      };
+      handleNavItemsAnimation();
+      navBtn.addEventListener("click", handleNav);
