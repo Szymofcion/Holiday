0b9e28b971a0dded7cfeab4b99927f09b1fa4ad4
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2022-02-17 19:55:01 +0100

    fixed: fix img and js nav

--- js/main.js
+++ js/main.js
@@ -1,30 +1,58 @@
 const nav = document.querySelector(".nav");
-      const navBtn = document.querySelector(".burger-btn");
-      const allNavItems = document.querySelectorAll(".nav__item");
-      
-      const handleNav = () => {
-        nav.classList.toggle("nav--active");
-        allNavItems.forEach((item) => {
-          item.classList.toggle("nav-items-animation");
-        });
-        allNavItems.forEach((item) => {
-          item.addEventListener("click", () => {
-            nav.classList.remove("nav--active");
-            allNavItems.forEach((item) => {
-              item.classList.remove("nav-items-animation");
-            });
-          });
-        });
-        
-      };
-      
-      const handleNavItemsAnimation = () => {
-        let delayTime = 0;
-      
-        allNavItems.forEach((item) => {
-          item.style.animationDelay = '.' + (delayTime + 2) + "s";
-          delayTime++;
-        });
-      };
-      handleNavItemsAnimation();
-      navBtn.addEventListener("click", handleNav);
+const navBtn = document.querySelector(".burger-btn");
+const allNavItems = document.querySelectorAll(".nav__item");
+const navBtnBars = document.querySelector(".burger-btn__bars");
+const allSections = document.querySelectorAll(".section");
+const fotterYear = document.querySelector(".footer__year");
+
+const handleNav = () => {
+  nav.classList.toggle("nav--active");
+  navBtnBars.classList.remove('black-bars-color');
+  allNavItems.forEach((item) => {
+    item.classList.toggle("nav-items-animation");
+  });
+  allNavItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      nav.classList.remove("nav--active");
+      allNavItems.forEach((item) => {
+        item.classList.remove("nav-items-animation");
+      });
+    });
+  });
+};
+
+const handleNavItemsAnimation = () => {
+  let delayTime = 0;
+
+  allNavItems.forEach((item) => {
+    item.style.animationDelay = "." + (delayTime + 2) + "s";
+    delayTime++;
+  });
+};
+const handleCurrentYear = () => {
+  const year = new Date().getFullYear();
+  fotterYear.innerText = year;
+};
+const handleObserver = () => {
+  const currentSection = window.scrollY;
+
+  allSections.forEach(section => {
+  
+    if(section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+      navBtnBars.classList.add('black-bars-color')
+    } else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+      navBtnBars.classList.remove('black-bars-color')
+    }
+
+
+  })
+}
+
+
+
+
+
+handleCurrentYear();
+handleNavItemsAnimation();
+navBtn.addEventListener("click", handleNav);
+window.addEventListener('scroll', handleObserver)
